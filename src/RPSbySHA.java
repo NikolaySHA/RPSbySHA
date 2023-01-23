b6f3d7dc6b16f57e33c42c1a22a7f64d2de7bc4b
NikolaySHA <nikolaishatov@gmail.com>
2023-01-23 23:14:53 +0200

    Version 1.0

--- src/RPSbySHA.java
+++ src/RPSbySHA.java
@@ -3,10 +3,7 @@ import java.util.Scanner;
 
 public class RPSbySHA {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-
-
         String[] moves = new String[3];
         moves[0] = "Rock";
         moves[1] = "Paper";
@@ -16,22 +13,14 @@ public class RPSbySHA {
         int countDraw = 0;
         int games = 0;
         String input = "y";
-
         System.out.println("Hello player, I'm RPSbySHA, a simple console \"Rock, Paper, Scissors\" game.");
-
         System.out.println("What's your name?");
-
         String player = scanner.nextLine();
-
         System.out.printf("OK %s, let's play a game!%n", player);
-
         while (!"n".equals(input) && !"N".equals(input) && !"No".equals(input) && !"no".equals(input)) {
-
             System.out.printf("Select one of the following options:%n\"[R]ock\"%n\"[P]aper\"%n" +
                     "\"[S]cissors\"%nPlease write your choice.%n");
-
             String playerTurn = scanner.nextLine();
-
             switch (playerTurn) {
                 case "Rock", "rock", "R", "r" -> playerTurn = moves[0];
                 case "Paper", "paper", "P", "p" -> playerTurn = moves[1];
@@ -45,12 +34,12 @@ public class RPSbySHA {
             int index = random.nextInt(moves.length);
             String SHATurn = (moves[index]);
             System.out.printf("Your pick is: %s%nRPSbySHA pick is: %s%n", playerTurn, SHATurn);
-
             if (playerTurn.equals(moves[0]) && SHATurn.equals(moves[2]) || playerTurn.equals(moves[1]) && SHATurn.equals(moves[0]) ||
                     playerTurn.equals(moves[2]) && SHATurn.equals(moves[1])) {
                 countPlayer++;
                 System.out.println("Congratulations, you win!");
-            } else if (playerTurn.equals(moves[2]) && SHATurn.equals(moves[0]) || playerTurn.equals(moves[0]) && SHATurn.equals(moves[1]) ||
+            }
+            else if (playerTurn.equals(moves[2]) && SHATurn.equals(moves[0]) || playerTurn.equals(moves[0]) && SHATurn.equals(moves[1]) ||
                     playerTurn.equals(moves[1]) && SHATurn.equals(moves[2])){
                 countSHA++;
                 System.out.println("Oh, you lost!");
@@ -60,32 +49,28 @@ public class RPSbySHA {
                 System.out.println("The game was a draw!");
             }
             System.out.printf("Do you want to play another round?%n[Y]es or [N]o%nPlease write your choice.%n");
-
             while (true) {
                 input = scanner.nextLine();
                 if (input.equals("n") || input.equals("N") || input.equals("no") || input.equals("No") ||
                         input.equals("y") || input.equals("Y") || input.equals("Yes") || input.equals("yes")) {
                     break;
-                } else {
+                }
+                else {
                     System.out.printf("Invalid input.[Y]es or [N]o%nPlease write your choice.%n");
                 }
             } games++;
-
         }
         if (countPlayer > countSHA) {
-
             System.out.printf("Congratulations you are the winner!%n%s: %d wins%nRPSbySHA: %d wins%nDraws: %d%n" +
                     "Total games: %d", player, countPlayer, countSHA, countDraw, games);
-        } else if (countPlayer < countSHA) {
+        }
+        else if (countPlayer < countSHA) {
             System.out.printf("I'm sorry but you lost!%n%s: %d wins%nRPSbySHA: %d wins%nDraws:%d%nTotal games: %d"
                     , player, countPlayer, countSHA, countDraw, games);
-
-        } else {
+        }
+        else {
             System.out.printf("Wow the game is tie!%n%s: %d wins%nRPSbySHA: %d wins%n%d draws%nTotal games:%d "
                     , player, countPlayer, countSHA, countDraw, games);
         }
-
-
     }
-
 }
